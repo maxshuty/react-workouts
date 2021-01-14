a3e0838206329ee03c1e4ff11b7b547a5b28a05d
Max Poshusta <max.poshusta@solutiondesign.com>
2021-01-14 10:47:52 -0600

    Adding comment to implement tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+// TODO: Max P - implement tests
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
