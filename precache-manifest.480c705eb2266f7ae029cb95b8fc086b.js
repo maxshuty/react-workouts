2efb0e5a4a4de3fef66c0123cc6f2aee3350b104
Max Poshusta <max.poshusta@solutiondesign.com>
2021-01-13 18:45:38 -0600

    Updates

---
+++ precache-manifest.480c705eb2266f7ae029cb95b8fc086b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b24802cd1f7f40bf517a7be8ef1291",
+    "url": "/react-workouts/index.html"
+  },
+  {
+    "revision": "66a8bd0f3ab55834a136",
+    "url": "/react-workouts/static/css/main.936d5451.chunk.css"
+  },
+  {
+    "revision": "743c642d1dd08585ebc3",
+    "url": "/react-workouts/static/js/2.5e62a50e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-workouts/static/js/2.5e62a50e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66a8bd0f3ab55834a136",
+    "url": "/react-workouts/static/js/main.7b16fa73.chunk.js"
+  },
+  {
+    "revision": "b57d784c4fe62594ba59",
+    "url": "/react-workouts/static/js/runtime-main.ef3ef0f0.js"
+  }
+]);
